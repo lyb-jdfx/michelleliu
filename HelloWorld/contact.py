e5a89b6fa9a3e8efc3389e8d632460271820f42a
lyb-jdfx <44003107+lyb-jdfx@users.noreply.github.com>
2019-05-30 19:34:19 +0100

    Add files via upload

---
+++ HelloWorld/contact.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+app = Flask("Projectcnc")
+
+@app.route("/home")
+def main_page():
+    return render_template("project_mainpage.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/event")
+def event():
+    return render_template("Latestevent.html")
+
+@app.route("/about")
+def about():
+    return render_template("aboutcodencoffee.html")
+
+@app.route("/contact_return", methods=["POST"])
+def contact_return_():
+    form_data=request.form
+    print form_data["gender"]
+    if form_data["gender"] == "female":
+        return "Welcome to the Code and Coffee Family!"
+    else:
+        return "Sorry, we only accept female members!"
+
+app.run(debug=True)
